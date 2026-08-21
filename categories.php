05be95d797f217cb9a777a636d6f36a992bff39e
VishanAaleMagar <boom.vishan00@gmail.com>
2026-08-22 00:13:25 +0545

    ew

--- categories.php
+++ categories.php
@@ -81,8 +81,8 @@ $ce = fn($v) => htmlspecialchars((string)$v, ENT_QUOTES, 'UTF-8');
 <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Nunito:wght@400;600;700;800;900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 <link rel="stylesheet" href="css/style.css?v=35">
-<link rel="stylesheet" href="css/categories-mobile.css?v=5">
-<link rel="stylesheet" href="css/cards-mobile.css?v=1">
+<link rel="stylesheet" href="css/categories-mobile.css?v=6">
+<link rel="stylesheet" href="css/cards-mobile.css?v=2">
 </head>
 <body>
 
