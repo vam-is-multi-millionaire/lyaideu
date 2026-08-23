dc1fa0b8bdced0cfcb29195e9ed976d8c528e543
VishanAaleMagar <boom.vishan00@gmail.com>
2026-08-23 17:59:37 +0545

    ok

--- categories.php
+++ categories.php
@@ -80,7 +80,7 @@ $ce = fn($v) => htmlspecialchars((string)$v, ENT_QUOTES, 'UTF-8');
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Nunito:wght@400;600;700;800;900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-<link rel="stylesheet" href="css/style.css?v=51">
+<link rel="stylesheet" href="css/style.css?v=52">
 <link rel="stylesheet" href="css/categories-mobile.css?v=8">
 <link rel="stylesheet" href="css/cards-mobile.css?v=15">
 </head>
