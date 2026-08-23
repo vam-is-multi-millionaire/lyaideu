7711398cff63c15e056673e55d2e35d98c50ed57
VishanAaleMagar <boom.vishan00@gmail.com>
2026-08-23 14:33:59 +0545

    few

--- menu.php
+++ menu.php
@@ -36,7 +36,7 @@ $ce = fn($v) => htmlspecialchars((string)$v, ENT_QUOTES, 'UTF-8');
 <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Nunito:wght@400;600;700;800;900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 <link rel="stylesheet" href="css/style.css?v=47">
-<link rel="stylesheet" href="css/cards-mobile.css?v=5">
+<link rel="stylesheet" href="css/cards-mobile.css?v=7">
 </head>
 <body>
 
